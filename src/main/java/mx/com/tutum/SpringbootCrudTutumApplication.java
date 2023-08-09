2f2a5bd20a901c7e6c1c5e2cb91900142273da3a
Eduardo S Santiago <sams.edu85@gmail.com>
2023-08-09 17:58:54 -0600

    Proyecto inicial

---
+++ src/main/java/mx/com/tutum/SpringbootCrudTutumApplication.java
@@ -0,0 +1,13 @@
+package mx.com.tutum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudTutumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudTutumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/tutum/SpringbootCrudTutumApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.tutum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudTutumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
