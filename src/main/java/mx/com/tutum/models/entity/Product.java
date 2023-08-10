d9545402905c77d643e7254c534c52b4368403ea
Eduardo S Santiago <sams.edu85@gmail.com>
2023-08-09 18:08:37 -0600

    se crea el modelo producto

---
+++ src/main/java/mx/com/tutum/models/entity/Product.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package mx.com.tutum.models.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author EduSam
+ *
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+public class Product implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7094636838296606312L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_product")
+	private Long idProduct;
+
+	private String name;
+	private Double price;
+
+	@Column(name = "create_at")
+	private LocalDateTime createAt;
+
+}
